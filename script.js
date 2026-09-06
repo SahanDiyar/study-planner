3dbc8e14a7d0341304c05164217493dc78dd5097
Sahan Diyar <sahandiyar4@gmail.com>
2026-09-06 08:54:56 +0300

    mixteral

--- script.js
+++ script.js
@@ -241,7 +241,7 @@ ${notes}`;
           "Content-Type": "application/json"
         },
         body: JSON.stringify({
-          model: "llama-3.1-8b-instant",
+          model: "mixtral-8x7b-32768",
           messages: [{ role: "user", content: prompt }]
         })
       });
@@ -641,7 +641,7 @@ ${notes}`;
           "Content-Type": "application/json"
         },
         body: JSON.stringify({
-          model: "llama-3.1-8b-instant",
+          model: "mixtral-8x7b-32768",
           messages: [{ role: "user", content: prompt }]
         })
       });
